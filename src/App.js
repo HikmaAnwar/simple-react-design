29a7210d9113caf499afbd952c2998767a378c22
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-06-25 21:42:24 +0300

    fix: minor change

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './login';
 import SignUp from './signup';
 import SourceCode from "./sourcecode";
 
+
 function App() {
   return (
     <>
--- src/home.jsx
+++ src/home.jsx
@@ -1,5 +1,12 @@
+import { useState } from 'react';
 
 const Home = () => {
+    const [buttonClicked, setButtonClicked] =useState(false);
+
+    const handeleButtonClick = () =>{
+        setButtonClicked(true);
+    };
+
     return(
         <div>
             <h1>We Train Best Standards of Responsive Websites And Apps</h1>
@@ -7,7 +14,12 @@ const Home = () => {
              rutrum leo pulvinar, pretium ligula. Maecenas consectetur sit amet quam at tincidunt.
              Pellentesque vel condimentum ante. Morbi in mauris vel felis malesuada congue.
              Morbi blandit porttitor odio vel consequat. Nulla gravida quis lorem vitae hendrerit.</p>
+            <p>Register Here:</p>
+            {buttonClicked ? <p>Button was clicked!</p> : null}
+             <button to="/signup" onClick={handeleButtonClick}>Sign Up</button>
         </div>
+
+        
     );
 };
 
