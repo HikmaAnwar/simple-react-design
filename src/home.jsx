64068242bd36a9340cce507ba9441622693d0d79
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-06-25 19:26:39 +0300

    feat: added home componenet

---
+++ src/home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return(
+        <div>
+            <h1>We Train Best Standards of Responsive Websites And Apps</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at quam finibus,
+             rutrum leo pulvinar, pretium ligula. Maecenas consectetur sit amet quam at tincidunt.
+             Pellentesque vel condimentum ante. Morbi in mauris vel felis malesuada congue.
+             Morbi blandit porttitor odio vel consequat. Nulla gravida quis lorem vitae hendrerit.</p>
+        </div>
+    )
+}
