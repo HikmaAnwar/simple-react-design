57af08a77e72d85480844901d041c35f65a92548
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-06-26 10:34:47 +0300

    feat: updated the signup component

--- src/signup.jsx
+++ src/signup.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setComfirmPassword] = useState('');
+
+    const handleSignup = () => {
+        console.log("signing up...", {username,email, password, confirmPassword});
+        setUsername('');
+        setEmail('');
+        setPassword('');
+        setComfirmPassword('');
+    }
+
+
     return(
         <div>
             <h1>signup Page</h1>
