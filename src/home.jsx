9205d8e7a472582dc95819e48a5870090e20f07b
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-06-26 10:06:17 +0300

    fix: minor change

--- src/home.jsx
+++ src/home.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [buttonClicked, setButtonClicked] =useState(false);
--- src/nav.jsx
+++ src/nav.jsx
@@ -1,28 +1,27 @@
-
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return(
-      <nav className="navbar">
-        <div className="navbar-brand">
-            <a to="/">Simple Design</a>
-        </div>
-
-         <ul className="navbar-links">
-            <li>
-                <a to="/">Home</a>
-            </li>
-            <li>
-                <a to="/sourcecode">Source Code</a>
-            </li>
-            <li> 
-                <a to="/login">Login</a>
-            </li>
-            <li> 
-                <a to="/signup">Signup</a>
-            </li>
-
-         </ul>
-    </nav>
-   );
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <Link to="/">Simple Design</Link>
+            </div>
+            <ul className="navbar-links">
+                <li>
+                    <Link to="/"><Home /></Link>
+                </li>
+                <li>
+                    <Link to="/sourcecode">Source Code</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                <li>
+                    <Link to="/signup">Signup</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 };
+
 export default NavBar;
