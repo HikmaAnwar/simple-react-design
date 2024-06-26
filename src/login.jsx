399fcd5bb29965dcdb45359cb1e35068ecb75b83
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-06-26 11:24:49 +0300

    feat: added a function to handle login

--- src/login.jsx
+++ src/login.jsx
@@ -1,5 +1,13 @@
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+
+    }
     return(
         <div>
             <h1>login page</h1>
