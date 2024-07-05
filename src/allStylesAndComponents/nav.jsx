d1c54c7f8ea9abfd28795b5f0420c6455fda878d
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-07-05 19:20:15 +0300

    fix: minor change

--- src/allStylesAndComponents/nav.jsx
+++ src/allStylesAndComponents/nav.jsx
@@ -1,23 +1,23 @@
 import { Link } from "react-router-dom";
-import styles from './nav.module.css';
+
 
 const NavBar = () => {
     return (
-        <nav className={styles.navbar}>
-            <div className={styles.navbarBrand}>
+        <nav className="flex justify-between items-center bg-white rounded-md p-2.5 text-black fixed mx-auto my-3 w-full pr-14 pl-14 shadow-xl">
+            <div className="text-green-700 font-sans no-underline font-bold size-14">
                 <Link to="/">Simple Design</Link>
             </div>
-            <ul className={styles.navbarLinks}>
-                <li>
+            <ul className="flex list-none space-x-4 justify-between ml-8 ">
+                <li className=" text-gray-500 hover:text-black">
                     <Link to="/">Home</Link>
                 </li>
-                <li>
+                <li className=" text-gray-500 hover:text-black">
                     <Link to="/sourcecode">Source Code</Link>
                 </li>
-                <li>
+                <li className=" text-gray-500 hover:text-black">
                     <Link to="/login">Login</Link>
                 </li>
-                <li>
+                <li className=" text-gray-500 hover:text-black">
                     <Link to="/signup">Signup</Link>
                 </li>
             </ul>
